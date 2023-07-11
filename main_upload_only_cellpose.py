643de2d9c8aaac53a419bbb4711051f37b8844c4
franz33 <jonas.franz@med.uni-goettingen.de>
2023-07-11 14:39:08 +0200

    updated for higher cuda, shapely 2.01

--- main_upload_only_cellpose.py
+++ main_upload_only_cellpose.py
@@ -1,10 +1,9 @@
 # This is to connect to omero
-from extract_cellpose import extract_cellpose_nucleiV2
+from upload_only_cellpose import upload_cellpose_nucleiV2
 import sys
 # This is the actual application of cellpose
 from get_histogram_dask import get_histogram_dask
 from save_omero_to_zarr import save_omero_to_zarr
-from save_channel_to_nib_file import save_channel_to_nib_file
 
 
 def pack_system_arguments():
@@ -13,20 +12,22 @@ def pack_system_arguments():
     user = "Franz"
     pw = sys.argv[1]
     imageId = sys.argv[2]
-    local = (sys.argv[3] == "True")
+    base = sys.argv[3]
+    gpu = (sys.argv[4]=="True")
     try:
-        c_fluorescence = int(sys.argv[4])
+        c_fluorescence = int(sys.argv[5])
     except IndexError:
         c_fluorescence = None
     try:
-        c_dapi = int(sys.argv[5])
+        c_dapi = int(sys.argv[6])
     except IndexError:
         c_dapi = None
     # pack sys argv
     sys_arguments = {"user": user,
                      "pw": pw,
                      "imageId": imageId,
-                     "local": local,
+                     "base": base,
+                     "gpu": gpu,
                      "c_fluorescence": c_fluorescence,
                      "c_dapi": c_dapi}
     return sys_arguments
@@ -39,7 +40,7 @@ def pack_parameters():
     overlap = 100
     evaluated_crop_size = maximum_crop_size - overlap
     width = 101
-    height: int = 101
+    height = 101
     half_width = int(width / 2.)
     half_height = int(height / 2.)
     parameters = {"maximum_crop_size": maximum_crop_size,
@@ -60,25 +61,21 @@ if __name__ == '__main__':
 
     parameters = pack_parameters()
 
-    #extraction = extract_cellpose_nucleiV2(sys_arguments, parameters)
-    #if extraction is 0:
-    #    print("Cell pose coordinates already extracted.")
-    #elif extraction is 1:
-    #    print("Cell pose coordinates are now extracted and saved to omero.")
+    extraction = upload_cellpose_nucleiV2(sys_arguments, parameters)
+    if extraction == 0:
+        print("Cell pose coordinates already extracted.")
+    elif extraction == 1:
+        print("Cell pose coordinates are now extracted and saved to omero.")
 
     zarr_storage = save_omero_to_zarr(sys_arguments, parameters)
-    if zarr_storage is 0:
+    if zarr_storage == 0:
         print("Zarr storage already initiated.")
-    elif zarr_storage is 1:
+    elif zarr_storage == 1:
         print("Zarr storage is initiated now saved in this file system.")
 
     hist_calculation = get_histogram_dask(sys_arguments, parameters)
     print(hist_calculation)
-    if hist_calculation is 0:
+    if hist_calculation == 0:
         print("CDF already extracted.")
-    elif hist_calculation is 1:
+    elif hist_calculation == 1:
         print("CDF is now extracted and saved to omero.")
-
-    nib_saving = save_channel_to_nib_file(sys_arguments, parameters)
-    print(nib_saving[0])
-
