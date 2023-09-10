b4ef3456661630629f8c0eca1b7af7d7c165f50d
franz33 <jonas.franz@med.uni-goettingen.de>
2023-09-10 19:50:10 +0200

    updated how omero_tools fom JonasTools imported. To get it directly from the pip install it does not require JonasTools.omero_tools but only omero_tools. Otherwise it looks for the available JonasTools directory which is not necessary with pip JonasTools.

--- Code/MainFunctions/extract_cellpose_nucleiV2.py
+++ Code/MainFunctions/extract_cellpose_nucleiV2.py
@@ -4,10 +4,10 @@ import time
 import numpy as np
 from scipy.stats import skew
 
-from Code.Utils.image_tools import get_coordinates, delete_cells_at_border
-from JonasTools.omero_tools import refresh_omero_session, get_image, get_pixels, get_tile_coordinates, UploadArrayAsTxtToOmero, \
+from Utils.image_tools import get_coordinates, delete_cells_at_border
+from omero_tools import refresh_omero_session, get_image, get_pixels, get_tile_coordinates, UploadArrayAsTxtToOmero, \
     check_fname_omero, make_omero_file_available
-from Code.Utils.utils import extract_system_arguments, unpack_parameters
+from Utils.utils import extract_system_arguments, unpack_parameters
 
 
 def extract_cellpose_nucleiV2(sys_arguments, parameters):
